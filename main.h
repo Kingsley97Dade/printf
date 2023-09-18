5899f0d0edb10b1c4f7d17a70a1475ad22163eb4
Code021-raelle <gabrielakinshola3@gmail.com>
2023-09-18 09:38:19 -0700

    _printf.c

--- main.h
+++ main.h
@@ -40,7 +40,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-		va_list list, char buffer[], int flags, int width int precision, int size);
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
@@ -111,7 +111,7 @@ int append_hexa_code(char, char[], int);
 int is_digit(char);
 
 long int convert_size_number(long int num, int size);
-long int convert_size_number(unsigned long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
 
